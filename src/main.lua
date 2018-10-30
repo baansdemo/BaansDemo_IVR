130dc7980fafdc5814f3a07d4ea46c4eb99221e1
baansdemo <baansdemo@gmail.com>
2018-10-30 12:32:00 +0530

    Sound check again

--- src/main.lua
+++ src/main.lua
@@ -2,16 +2,23 @@
 -- coverage: 0
 channel.answer()
 
---channel.play("assets://sounds/Titanic.wav")
+channel.play("assets://sounds/Titanic.wav")
 
+function demofunction()
+channel.say("Thank you for choosing us. Please press 1 to listen again and 2 to talk to our expert.")
 
-channel.play({'/sounds/GPV.mp3', 'assets://sounds/Titanic.wav'})
+local digit = channel.gather()
 
-local speech = require('summit.speech')
-local x = speech('Hello!')
-channel.play(x, {loop=3})
+if digit == '1' then
+	print(demofunction())
 
-local result
-if err == nil and result then
-    channel.say('You entered ' .. result)
+else if digit == '2' then
+
+else 
+channnel.say("Please press either 1 or 2.")
 end
+end
+channel.hangup()
+end
+
+print(demofunction())

7f187e1094bc218c0dd1acd33594cba091df1db4
baansdemo <baansdemo@gmail.com>
2018-11-01 18:36:30 +0530

    01 Nov

--- src/main.lua
+++ src/main.lua
@@ -1,11 +1,16 @@
 channel.answer()
 
 --local log = require("summit.log")
+
+local dnis = channel.data.dnis
+channel.say(dnis)
+
 local application = require("summit.application")
+
 local number = application.get_destination()
 channel.say(number)
---log.info(app_dest)
 
+-- log.info(app_dest)
 -- local dss = channel.data.dnis
 -- channel.say(dss)
 -- channel.dial(channel.data.dnis)

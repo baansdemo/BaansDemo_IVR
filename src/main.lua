81e878d7fc7323c0429e834bcb2057c141c552ce
baansdemo <baansdemo@gmail.com>
2018-11-01 17:52:17 +0530

    1 Sep

--- src/main.lua
+++ src/main.lua
@@ -1,3 +1,13 @@
 channel.answer()
-channel.dial(channel.data.dnis)
+
+-- local log = require("summit.log")
+-- local application = require("summit.application")
+-- local app_dest = application.get_destination()
+-- print(app_dest)
+-- log.info(app_dest)
+
+--local dss = channel.data.dnis
+--channel.say(dss)
+-- channel.dial(channel.data.dnis)
+
 local functions = require('audio')

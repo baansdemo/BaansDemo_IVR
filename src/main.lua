17367a93222e86630d23b3f7b2b77672bd317d2a
baansdemo <baansdemo@gmail.com>
2018-11-02 17:15:19 +0530

    02Nov

--- src/main.lua
+++ src/main.lua
@@ -37,6 +37,51 @@ local timecalc_over_1 = ''
 local TestCall = '1' -- set to 1 to mark calls as tests
 --End SwamiVision Variables
 
+
+--Generic Functions
+function FailedCallTroubleFunction( ... )
+   channel.play('asset://InvalidInputGoodbye.wav')
+   channel.hangup()
+end
+
+--End Generic Functions
+
+--call finalization functions
+function finalizeCall()
+    local successfulSubject = 'Successful IVR Call '
+    local success_to_email = 'nikhilsaini5748@gmail.com'--'caytonr@voipswami.com'
+    --close call in SwamiVision if not done already
+    if MemberToMain == 'no' then
+        SwamiVisionCloseCall(SwamiVisionHangup, SwamiVisionTimeStamp())
+    end
+    if timecalc_over_1 == 'yes' then
+        successfulSubject = '*****' .. successfulSubject
+    end
+
+    --email if there is any sort of api failure
+    if SwamiVisionFailureType == '' then
+        --do nothing the call was a success
+        --actually email results with API performance over 1 second
+        if timecalc_over_1 == 'yes' then
+            for to_address in string.gmatch(success_to_email,"([^,]+)") do
+                email.send(success_to_email, from_email, successfulSubject .. AccountName .. ' ' .. IVRName, debug_results)
+            end
+        else
+            --do nothing
+        end
+    else
+        --email us the error(s)
+        for to_address in string.gmatch(to_email,"([^,]+)") do
+            email.send(to_address, from_email, subject_email ..' ' .. AccountName .. ' ' .. IVRName, SwamiVisionFailureType .. '\r\n' .. debug_results)
+        end
+    end
+end
+
+--call cleanup to process data received even if call disconnects early or there is a script issue
+cleanup.register(finalizeCall, 'finalizeCall', false)
+
+--end call finalization
+
 --SwamiVision specific functions
 --saves the caller ID to the correct variable for SwamiVision
 function readCID(CID)
@@ -46,7 +91,6 @@ function readCID(CID)
     else
         CallerIDSwamiVision = CID
     end
-    channel.say("cid number"..CallerIDSwamiVision)
     writeDebugResult('Start Time Z: ' .. SwamiVisionTimeStamp())
     writeDebugResult('Start Time CST: ' .. tostring(time.now('US/Central')))
     writeDebugResult('Callerid: ' .. CallerIDSwamiVision)
@@ -68,12 +112,9 @@ function SwamiVisionGetGUID( ... )
     url = urlGUID
 
     startTimeCalc = time.to_unix_ts(time.now("UTC"))
-    channel.say("start time"..startTimeCalc)
     log.info("Making request to "..url.." with begin time: "..BeginTime.." GetGUIDCall")
 
     r,err = http.get(url, {data=params,timeout=SwamiVisionAPITimeout})
-
-    channel.say("r message is"..r)
 
     --log.info("HTTP Response content: "..r.content.." status code: "..r.statusCode.." reason: "..r.reason)
     endTimeCalc = time.to_unix_ts(time.now("UTC"))
@@ -106,16 +147,186 @@ function SwamiVisionGetGUID( ... )
     end
 end
 
+--submits a call action to SwamiVision
+function SwamiVisionAddCallAction( PromptGUID, BeginTime )
+    CallActionGUID = 'reset'
+    if CallGUID ~= nil then
+        writeDebugResult('-----------------------------------------------------------------')
+        local startTimeCalc
+        local endTimeCalc
+        params = {CallGUID=CallGUID, BeginTime=BeginTime, PromptGUID=PromptGUID}
+        url = urlCreateAction
+
+        startTimeCalc = time.to_unix_ts(time.now("UTC"))
+        log.info("Making request to "..url.." with begin time: "..BeginTime.." and prompt GUID:"..PromptGUID)
+
+        r,err = http.get(url, {data=params,timeout=SwamiVisionAPITimeout})
+
+        --log.info("HTTPResponse content: "..r.content.." status code: "..r.statusCode.." reason: "..r.reason)
+        endTimeCalc = time.to_unix_ts(time.now("UTC"))
+        writeDebugResult('Add Action: ' .. (endTimeCalc - startTimeCalc))
+
+        if tonumber(endTimeCalc - startTimeCalc) > 1 then
+            timecalc_over_1 = 'yes'
+        end
+
+        if not err then
+
+            local data = string.gsub(r.content,'"','')
+
+            if not data then
+                writeDebugResult('No data received during Create Action' .. CallGUID .. '   ' .. PromptGUID)
+                api_failed_SwamiVision()
+            else
+                if data == '{Message:An error has occurred.}' or data == 'error' or string.sub(data,1,9) == '<!DOCTYPE' or data == '' then
+                    writeDebugResult('Error receiving Action GUID ' .. string.sub(data,1,40))
+                    writeDebugResult(PromptGUID .. ' ' .. PromptGUID .. ' ' .. PromptGUID)
+                    api_invalid_data(string.sub(data,1,40), 'GetActionGUID Failure')
+                else
+                    CallActionGUID = data
+                    writeDebugResult('CallActionGUID: ' .. data)
+                end
+            end
+        else
+            writeDebugResult('No data received during Create Action - web connection failure ' .. CallGUID .. '   ' .. PromptGUID)
+            writeDebugResult(err)
+            api_failed_SwamiVision()
+        end
+    else
+        --do nothing because the call GUID is invalid
+    end
+end
+
+--submits a call action to SwamiVision
+function SwamiVisionCloseCallAction( TSCallActionGUID,ActionValue,TimeStamp )
+    if CallGUID ~= nil and TSCallActionGUID ~= 'reset' then
+        local startTimeCalc
+        local endTimeCalc
+        params = {CallActionGUID=TSCallActionGUID, ActionValue=ActionValue, EndTime=TimeStamp}
+        url = urlCloseAction
+
+        startTimeCalc = time.to_unix_ts(time.now("UTC"))
+        log.info("Making request to "..url.." with begin time: "..TimeStamp.." and call action GUID:"..TSCallActionGUID)
+
+        r,err = http.get(url, {data=params,timeout=SwamiVisionAPITimeout})
+
+        --log.info("HTTPResponse content: "..r.content.." status code: "..r.statusCode.." reason: "..r.reason)
+        endTimeCalc = time.to_unix_ts(time.now("UTC"))
+        writeDebugResult('Close action: ' .. (endTimeCalc - startTimeCalc))
+
+        if tonumber(endTimeCalc - startTimeCalc) > 1 then
+            timecalc_over_1 = 'yes'
+        end
+
+        if not err then
+
+            local data = string.gsub(r.content,'"','')
+
+            if not data then
+                writeDebugResult('No data received during close call action ' .. CallGUID .. '   ' .. TSCallActionGUID)
+                api_failed_SwamiVision()
+            else
+                if data == 'ok' then
+                --channel.say(data)
+                else
+                    --data received is in error
+                    writeDebugResult('Error writing call action Close to API for PromptGUID: ' .. TSCallActionGUID .. ' ' .. ActionValue .. ' ' .. TimeStamp)
+                    api_invalid_data(string.sub(data,1,40), 'CallActionGUID: ' .. TSCallActionGUID)
+                end
+            end
+        else
+            writeDebugResult('No data received during close call action - web connection failure ' .. CallGUID .. '   ' .. TSCallActionGUID)
+            writeDebugResult(err)
+            api_failed_SwamiVision()
+        end
+    else
+        --do nothing because the call GUID or action GUID is invalid so we can't write data
+    end
+    --reset the call action GUID so it doesn't try to close action again if a get GUID failure
+    CallActionGUID = 'reset'
+end
+
+
+--submits close call action to SwamiVision
+function SwamiVisionCloseCall( Hangup,TimeStamp )
+    if CallGUID ~= nil then
+        writeDebugResult('-----------------------------------------------------------------')
+        local startTimeCalc
+        local endTimeCalc
+        local closeRetryCount = 0
+        params = {CallGUID=CallGUID, PasswordHash=PasswordHash, Hangup=Hangup, EndTime=TimeStamp}
+        url = urlClose
+
+        --try closing the call three times since it wont affect the callers perception of flow
+        while closeRetryCount < 3 do
+            startTimeCalc = time.to_unix_ts(time.now("UTC"))
+            log.info("Making request to "..url.." with begin time: "..TimeStamp.." Close Call Action")
+
+            r,err = http.get(url, {data=params,timeout=30})
+
+            --log.info("HTTPResponse content: "..r.content.." status code: "..r.statusCode.." reason: "..r.reason)
+            endTimeCalc = time.to_unix_ts(time.now("UTC"))
+            writeDebugResult('Close call: ' ..  (endTimeCalc - startTimeCalc))
+
+            if tonumber(endTimeCalc - startTimeCalc) > 1 then
+                timecalc_over_1 = 'yes'
+            end
+
+            if not err then
+                break
+            else
+                closeRetryCount = closeRetryCount + 1
+            end
+        end
+
+        if not err then
+
+            local data = string.gsub(r.content,'"','')
+
+            if not data then
+                writeDebugResult('No data received during close call action after ' .. (closeRetryCount + 1) .. ' tries')
+                api_failed_SwamiVision()
+            else
+                if data == 'ok' then
+                    --do nothing, call closed successfully
+                    writeDebugResult('Number of close call attempts: ' .. (closeRetryCount + 1))
+                else
+                    --{Message:An error has occurred.}' or data == 'error'
+                    writeDebugResult('Error closing call ' .. string.sub(data,1,40))
+                    api_invalid_data(string.sub(data,1,40),'Closing Call')
+                end
+            end
+        else
+            writeDebugResult('No data received during close call action - web connection error')
+            writeDebugResult('Number of close tries' .. (closeRetryCount + 1))
+            writeDebugResult(err)
+            api_failed_SwamiVision()
+        end
+    else
+        --do nothing because the call GUID is invalid and closing the call is pointless
+    end
+end
+
+--api call failed miserably
+function api_failed_SwamiVision( ... )
+    SwamiVisionFailureType = SwamiVisionFailureType .. "api failure \r\n"
+end
+
+--api call worked but we sent something over we shouldn't have
+function api_invalid_data( data, additionalData )
+    SwamiVisionFailureType = SwamiVisionFailureType .. 'api returned invalid data. ' .. data .. ' ' .. additionalData .. '\r\n'
+end
+
 --return timestamp with proper formatting
 function SwamiVisionTimeStamp()
     return (string.gsub(tostring(time.now("UTC")),' ', 'T') .. 'Z')
 end
 --end SwamiVision functions
 
---api call failed miserably
-function api_failed_SwamiVision( ... )
-    SwamiVisionFailureType = SwamiVisionFailureType .. "api failure \r\n"
-end
+
+
+
+--Example of an application start to finish:
 
 -----Start of the call processing functions-----
 ---Initial answer and web connection---
@@ -125,7 +336,26 @@ function AppStart( ... )
     return TestMenu
 end
 
+function TestMenu( ... )
+    SwamiVisionAddCallAction('123123123-BB7E-440B-9ECF-2777CFF4FF3F', SwamiVisionTimeStamp())
+    local MyTestMenu = channel.gather({maxDigits=1, attempts=1, timeout=3})--, play=audio_constants.TestMenuAudio, regex='[12]', invalidPlay=audio_constants.blank_audio
+        if MyTestMenu == "1" then
+            writeDebugResult('23123123-BB7E-440B-9ECF-2777CFF4FF3F' .. ' ' ..  MyTestMenu .. ' ' .. SwamiVisionTimeStamp())
+            SwamiVisionCloseCallAction(CallActionGUID, MyTestMenu, SwamiVisionTimeStamp())
+            return NextFunction1
+        elseif MyTestMenu == '2' then
+            writeDebugResult('23123123-BB7E-440B-9ECF-2777CFF4FF3F' .. MyTestMenu .. SwamiVisionTimeStamp())
+            SwamiVisionCloseCallAction(CallActionGUID, MyTestMenu, SwamiVisionTimeStamp())
+            return NextFunction2
+        else
+            writeDebugResult('23123123-BB7E-440B-9ECF-2777CFF4FF3F' .. ' Caller did not make a selection' .. SwamiVisionTimeStamp())
+            SwamiVisionCloseCallAction(CallActionGUID, ' Caller did not make a selection', SwamiVisionTimeStamp())
+            return FailedCallTroubleFunction
+        end
+end
+
 --The below answers the call and calls the first function to start the process off--
+
 channel.answer()
 
 readCID(channel.data.ani)
